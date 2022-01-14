8f99e7f66cb027b48ac5b75bd1b4ccbda725bf74
unknown <hemidovhormet2@mail.com>
2022-01-14 23:43:17 +0400

    HRMbuild

---
+++ src/components/Sec12UnderFooter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Sec12UnderFooter() {
+    return (
+        <div className="S12-Under-Footer dflex-1024">
+            <div className="dflex full-center w-100">
+                <div className="dflex sbetween w74">
+                    <div>
+                        <div>&copy; 2021. Bütün hüquqlar qorunmur. Veb səhifə <span>Hörmət Həmidov</span> tərəfindən yaradılmışdır</div>
+                    </div>
+                    <div className="dflex full-center">
+                        <div className="sonlink"><a className="instagram" rel="noreferrer" href="https://www.instagram.com/hemidovh_/" target="_blank"><i className="fab fa-instagram"></i></a></div>
+                        <div className="sonlink"><a className="facebook" rel="noreferrer" href="https://www.facebook.com/hormet.hemidov/" target="_blank"><i className="fab fa-facebook-f"></i></a></div>
+                        <div className="sonlink"><a className="telegram" rel="noreferrer" href="https://t.me/hormethemidov" target="_blank"><i className="fab fa-telegram-plane"></i></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Sec12UnderFooter;
